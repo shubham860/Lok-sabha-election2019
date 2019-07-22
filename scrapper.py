e55b8a1f636d8b6ae7fcdcfd0c833aea4616843d
shubham860 <shubham.chauhan860@gmail.com>
2019-07-23 03:58:08 +0530

    update

--- scrapper.py
+++ scrapper.py
@@ -5,9 +5,16 @@ from selenium import webdriver
 driver = webdriver.Chrome("C:\\usr\\chromedriver")
 
 
+def max_votes(n):
+    for j in range(1,len(n),2):
+           temp2.append(n[j].text)
+
 cities = []
 total_votes = []
-  
+winner_votes = []
+temp1 = [] 
+temp2 = []
+
 for i in range(1,81):
     url = "http://results.eci.gov.in/pc/en/constituencywise/ConstituencywiseS241.htm?ac={}".format(i)
     driver.get(url)
@@ -20,6 +27,10 @@ for i in range(1,81):
     
     total = soup.findAll('td',attrs={'style':'width:12%;'})
     total_votes.append(total[3].text)
+
+    votes = soup.findAll('td',attrs={'style':'width:13%;'})
+    max_votes(votes)
+    
     
     
       
